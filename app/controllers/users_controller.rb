33205b6327e7196659365e585d633cf4cb9b650e
VietLe <vietqle82@gmail.com>
2014-06-18 15:10:16 -0700

    fixed form submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,7 @@ class UsersController < ApplicationController
 		user.password = params[:user][:password]
 		user.password = params[:user][:password_confirmation]
 		user.save!
-		session[:user_id] = user.id.to_s
-		redirect_to '/'
+		redirect_to '/login'
 	end
 
 	def show
